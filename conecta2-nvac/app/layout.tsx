31e28a68ce4c257c14c8c5925478b3afbd01887a
Unkoll <fel.zamorano@duocuc.cl>
2023-11-27 14:36:52 -0400

    Revert "avance creación y modificación de servidores"
    This reverts commit 98f24d4242e776e451071e388c6ee9ba3f6a54c2.

--- conecta2-nvac/app/layout.tsx
+++ conecta2-nvac/app/layout.tsx
@@ -1,11 +1,9 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
-
-import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ModalProvider } from '@/components/providers/modal-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -30,7 +28,6 @@ export default function RootLayout({
           defaultTheme='dark'
           enableSystem={true}
           storageKey='conecta2-theme'>
-          <ModalProvider />
           {children}
         </ThemeProvider>
         </body>

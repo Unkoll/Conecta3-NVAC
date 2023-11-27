2eec1be0585bd9ec12b3d7d0fe196739663183db
Unkoll <fel.zamorano@duocuc.cl>
2023-11-27 14:37:49 -0400

    Revert "Revert "avance creación y modificación de servidores""
    This reverts commit 31e28a68ce4c257c14c8c5925478b3afbd01887a.

---
+++ conecta2-nvac/components/server/server-header.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { ServerWithMembersWithProfiles } from "@/types"
+import { MemberRole } from "@prisma/client"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, User, UserPlus } from "lucide-react"
+import { useModal } from "@/hooks/use-modal-store"
+
+interface ServerHeaderProps {
+    server: ServerWithMembersWithProfiles
+    role?: MemberRole
+}
+
+export const ServerHeader = ({
+    server,
+    role
+}: ServerHeaderProps) => {
+    const { onOpen } = useModal();
+
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger
+                className="focus:outline-none"
+                asChild>
+                    <button
+                        className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+                            {server.name}
+                            <ChevronDown className="h-5 w-5 ml-auto"/>
+                    </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+                    {isModerator && (
+                        <DropdownMenuItem
+                            onClick={() => onOpen("invite", {server: server})}
+                            className="text-violet-600 dark:text-violet-400 px-3 py-2 text-sm cursor-pointer">
+                            Invitar al servidor
+                            <UserPlus className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+                    {isAdmin && (
+                        <DropdownMenuItem
+                            onClick={() => onOpen("editServer", { server })}
+                            className="px-3 py-2 text-sm cursor-pointer">
+                            Ajustes del servidor
+                            <Settings className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+                    {isAdmin && (
+                        <DropdownMenuItem
+                            className="px-3 py-2 text-sm cursor-pointer">
+                            Ver miembros
+                            <User className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+                    {isModerator && (
+                        <DropdownMenuItem
+                            className="px-3 py-2 text-sm cursor-pointer">
+                            Crear canal
+                            <PlusCircle className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+                    {isModerator && (
+                        <DropdownMenuSeparator />
+                    )}
+                    {isAdmin && (
+                        <DropdownMenuItem
+                            className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                            Borrar servidor
+                            <Trash className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+                    {!isAdmin && (
+                        <DropdownMenuItem
+                            className="px-3 py-2 text-sm cursor-pointer">
+                            Abandonar servidor
+                            <LogOut className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                    )}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
